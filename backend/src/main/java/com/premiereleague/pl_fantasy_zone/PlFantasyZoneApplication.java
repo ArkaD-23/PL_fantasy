093f1adf035497f1267519ce3fe732793b301691
Arkaprava Dhar <raj2003dhar@gmail.com>
2024-11-13 09:06:33 +0530

    Datasource added and backend running

---
+++ backend/src/main/java/com/premiereleague/pl_fantasy_zone/PlFantasyZoneApplication.java
@@ -0,0 +1,13 @@
+package com.premiereleague.pl_fantasy_zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlFantasyZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlFantasyZoneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/premiereleague/pl_fantasy_zone/PlFantasyZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premiereleague.pl_fantasy_zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlFantasyZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
